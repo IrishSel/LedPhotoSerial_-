1724bd7f389132e2c6c2af7b005723655b19bb08
IrishSel <115017524+IrishSel@users.noreply.github.com>
2022-12-14 15:30:31 +0800

    Add files via upload

---
+++ aduino.py
@@ -0,0 +1,40 @@
+import serial
+import time
+
+lengths = {'s': 3,
+           'u': 0,
+           'd': 0}
+
+def get_connection(port):
+    ser = serial.Serial(port, timeout = 1)
+    return ser
+
+def send (ser,message,mesg_len):
+    ser.write(message)
+    time.sleep(0.1)
+    result = None
+    if mesg_len != 0:
+        data = ser.read(mesg_len)
+        result = data.decode()
+        result = result.strip()
+        print(result)
+    return result
+
+Ther_holds = 850
+if __name__ == '__main__':
+    ser = get_connection("COM10")
+    while True:
+        inp = input("Enter command: ")
+        #length = lengths.get(inp, 17)
+        #send(ser, inp.encode(), length)
+        cmd = 's'
+        val = send(ser, cmd.encode(), lengths[cmd])
+
+        if val:
+            val = int(val)
+            print(f'Val as int {val}')
+            if val < Ther_holds:
+                send(ser, 'u'.encode(), 0)
+            else:
+                send(ser, 'd'.encode(), 0)
+        time.sleep(1)
